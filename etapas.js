d2c4683d13c12a830c52e128a98b7e25a7264e72
lucasmessias322 <lucasmessiaspereira322@gmail.com>
2021-01-20 20:04:31 -0300

    frist commit

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador', }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: '77222.jpg', legenda: 'vereador' }
+                ]
+            },
+
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Cliclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: '99.jpg', legenda: 'Prefeito', small: false },
+                    { url: '99_2.jpg', legenda: 'Vice-prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: '84.jpg.jpg', legenda: 'Prefeito', small: false },
+                    { url: '84_2.jpg', legenda: 'Vice-prefeito', small: true }
+                ]
+            },
+
+        ]
+    },
+]
